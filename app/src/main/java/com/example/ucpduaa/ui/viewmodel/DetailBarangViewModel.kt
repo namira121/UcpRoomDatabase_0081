373c6cc6a4e77b9706122e8e0903ea9186bd364a
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 20:57:38 +0700

    membuat state untuk menampilkan detail data barang

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/DetailBarangViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/DetailBarangViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.ucpduaa.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.ucpduaa.data.entity.Barang
+import com.example.ucpduaa.repository.RepositoryBarang
+import com.example.ucpduaa.ui.navigation.DestinasiDetailBarang
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 fun Barang.toDetailEvent(): BarangEvent{
     return BarangEvent(
@@ -24,4 +31,20 @@ data class DetailBrgUIState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != BarangEvent()
+}
+
+class DetailBarangViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBarang: RepositoryBarang,
+): ViewModel(){
+    private val _id: String = checkNotNull(savedStateHandle[DestinasiDetailBarang.ID])
+
+    val detailbrgUIState: StateFlow<DetailBrgUIState> = repositoryBarang.getBarang(_id)
+        .filterNotNull()
+        .map {
+            DetailBrgUIState(
+                detailUiEvent = it.toDetailEvent(),
+                isLoading = false,
+            )
+        }
 }
