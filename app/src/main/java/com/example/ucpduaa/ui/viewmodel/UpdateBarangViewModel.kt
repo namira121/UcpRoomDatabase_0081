c4c891159d78ae006f9eb33eeb0372accef06f2c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 22:00:50 +0700

    membuat class UpdateBarangViewModel untuk mengupdate data sesuai id

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/UpdateBarangViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/UpdateBarangViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.ucpduaa.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.ucpduaa.data.entity.Barang
+import com.example.ucpduaa.repository.RepositoryBarang
+import com.example.ucpduaa.ui.navigation.DestinasiUpdateBarang
 
 fun Barang.toUIStateBrg():BrgUIState = BrgUIState(
     barangEvent = this.toDetailEvent(),
 )
+
+class UpdateBarangViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBarang: RepositoryBarang
+):ViewModel(){
+    var updateUIState by mutableStateOf(BrgUIState())
+        private set
+
+    private val _id: Int = checkNotNull(savedStateHandle[DestinasiUpdateBarang.ID])
+
+}
