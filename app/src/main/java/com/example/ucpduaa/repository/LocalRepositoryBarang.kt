ed169470b57c8459732540c655d82c5d329b3516
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:06:32 +0700

    mengimplementasikan fungsi getAllbarang dari repositorybarang dan dao

--- app/src/main/java/com/example/ucpduaa/repository/LocalRepositoryBarang.kt
+++ app/src/main/java/com/example/ucpduaa/repository/LocalRepositoryBarang.kt
@@ -2,11 +2,16 @@ package com.example.ucpduaa.repository
 
 import com.example.ucpduaa.data.dao.BarangDao
 import com.example.ucpduaa.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryBarang(
     private val barangDao: BarangDao
 ):RepositoryBarang {
     override suspend fun insertbarang(barang: Barang) {
         barangDao.insertbarang(barang)
+    }
+
+    override fun getAllBarang(): Flow<List<Barang>> {
+        return barangDao.getAllBarang()
     }
 }
