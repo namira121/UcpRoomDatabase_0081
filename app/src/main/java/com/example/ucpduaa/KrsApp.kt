06ffdd0fc741d7dbf2619e6ef25dba08b14c5cc0
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 15:44:27 +0700

    menyimpan instance ContainerApp

--- app/src/main/java/com/example/ucpduaa/KrsApp.kt
+++ app/src/main/java/com/example/ucpduaa/KrsApp.kt
@@ -1,4 +1,8 @@
 package com.example.ucpduaa
 
-class KrsApp {
+import android.app.Application
+import com.example.ucpduaa.dependenciesinjection.ContainerApp
+
+class KrsApp: Application() {
+    lateinit var ContainerApp: ContainerApp
 }
