20672195938ee13b06681e1ccec6854a5d7b5e85
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:24:18 +0700

    membuat button untuk kembali

--- app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
@@ -1,8 +1,11 @@
 package com.example.ucpduaa.ui.customwidget
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,6 +24,18 @@ fun TopAppBar(
             .padding(16.dp),
         contentAlignment = Alignment.Center
     ){
-
+        if(showBackButton){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = onBack,
+                    modifier= Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("Back")
+                }
+            }
+        }
     }
 }
