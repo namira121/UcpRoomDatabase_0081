64058e98f1cfad96f2876043c3a232b1f02e4805
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 18:50:49 +0700

    menyimpan input form ke dalam entity

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/InsertBarangViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/InsertBarangViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.ucpduaa.ui.viewmodel
 
+import com.example.ucpduaa.data.entity.Barang
+
 data class BarangEvent(
     val nama: String = "",
     val deskripsi: String = "",
     val harga: String = "",
     val stok: String = "",
     val namaspl: String = "",
+)
+
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    nama= nama,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok,
+    namspl = namaspl
 )
