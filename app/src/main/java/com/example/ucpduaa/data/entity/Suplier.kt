f00919b60589796549c5f23eb55c5faaa073292f
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:35:54 +0700

    membuat tabel Suplier beserta atributnya

--- app/src/main/java/com/example/ucpduaa/data/entity/Suplier.kt
+++ app/src/main/java/com/example/ucpduaa/data/entity/Suplier.kt
@@ -1,4 +1,13 @@
 package com.example.ucpduaa.data.entity
 
-class Suplier {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "suplier")
+data class Suplier (
+    @PrimaryKey
+    val idspl:String,
+    val namaspl: String,
+    val kontakspl: String,
+    val alamatspl: String
+)
