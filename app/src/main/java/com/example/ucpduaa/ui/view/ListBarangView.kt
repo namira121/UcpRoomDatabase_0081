71e1e0c309d8c8b50d546c932535fd57312ccc80
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 17:13:38 +0700

    membuat fungsi BodyListBarangView dan menambahkan circular progress saat state loading

--- app/src/main/java/com/example/ucpduaa/ui/view/ListBarangView.kt
+++ app/src/main/java/com/example/ucpduaa/ui/view/ListBarangView.kt
@@ -1,8 +1,10 @@
 package com.example.ucpduaa.ui.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,15 +14,21 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucpduaa.data.entity.Barang
+import com.example.ucpduaa.ui.viewmodel.ListBrgUIState
 
 @Composable
 fun CardBarang(
@@ -91,3 +99,24 @@ fun ListBarang(
         )
     }
 }
+
+@Composable
+fun BodyListBarangView(
+    listbrgUIState: ListBrgUIState,
+    onClick: (Int) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        listbrgUIState.isLoading -> {
+            Box(
+                modifier=modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
+
