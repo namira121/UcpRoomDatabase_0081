d6a65c0fcd9670e58417f9e198a7c7d9b4cee186
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 16:41:42 +0700

    membuat card sebagai container data barang

---
+++ app/src/main/java/com/example/ucpduaa/ui/view/ListBarangView.kt
@@ -0,0 +1,31 @@
+package com.example.ucpduaa.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ucpduaa.data.entity.Barang
+
+@Composable
+fun CardBarang(
+    brg: Barang,
+    modifier: Modifier=Modifier,
+    onClick: () -> Unit = { }
+){
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row(mod) {  }
+        }
+    }
+}
