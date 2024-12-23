2ddc243f8bd27024915d46faff0a24dda03e1862
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:43:39 +0700

    membuat fungsi insertsuplier  untuk menambahkan data suplier di database

--- app/src/main/java/com/example/ucpduaa/data/dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucpduaa/data/dao/SuplierDao.kt
@@ -1,4 +1,13 @@
 package com.example.ucpduaa.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.example.ucpduaa.data.entity.Suplier
+
+@Dao
 interface SuplierDao {
+    @Insert
+    suspend fun insertsuplier(
+        suplier: Suplier
+    )
 }
