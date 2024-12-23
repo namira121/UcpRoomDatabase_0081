eb5a37dd877e9db4827cc940c39a017d665b1f23
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:42:33 +0700

    membuat fungsi updateBarang  untuk memperbarui data barang di database

--- app/src/main/java/com/example/ucpduaa/data/dao/BarangDao.kt
+++ app/src/main/java/com/example/ucpduaa/data/dao/BarangDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.ucpduaa.data.entity.Barang
 import kotlinx.coroutines.flow.Flow
 
@@ -22,4 +23,7 @@ interface BarangDao {
 
     @Delete
     suspend fun deleteBarang(barang: Barang)
+
+    @Update
+    suspend fun updateBarang(barang: Barang)
 }
