68b163531a7121cc101219e56323eae08e32d77b
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 02:20:26 +0700

    membuat state untuk list suplier yaitu state ketika loading, error dan menampilkan data

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/ListSuplierViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/ListSuplierViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.ucpduaa.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucpduaa.data.entity.Barang
 import com.example.ucpduaa.data.entity.Suplier
+import com.example.ucpduaa.repository.RepositoryBarang
+import com.example.ucpduaa.repository.RepositorySuplier
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class ListSplUIState(
     val listSpl: List<Suplier> = listOf(),
@@ -9,3 +21,36 @@ data class ListSplUIState(
     val isError: Boolean = false,
     val errorMessage: String = ""
 )
+
+class ListSuplierViewModel(
+    private  val repositorySuplier: RepositorySuplier
+): ViewModel(){
+    val listsplUIState: StateFlow<ListSplUIState> = repositorySuplier.getAllSuplier()
+        .filterNotNull()
+        .map {
+            ListSplUIState(
+                listSpl = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(ListSplUIState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                ListSplUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?:"Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ListSplUIState(
+                isLoading = true
+            )
+        )
+}
