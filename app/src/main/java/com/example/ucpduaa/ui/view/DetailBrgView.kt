0ce39e6d177d52bb678fad307022ba69104d8fdc
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 01:33:06 +0700

    Ketika state sedang loading, akan menampilkan circularprogressindicator

--- app/src/main/java/com/example/ucpduaa/ui/view/DetailBrgView.kt
+++ app/src/main/java/com/example/ucpduaa/ui/view/DetailBrgView.kt
@@ -1,9 +1,12 @@
 package com.example.ucpduaa.ui.view
 
 import android.app.AlertDialog
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +16,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -69,7 +76,19 @@ fun BodyDetailBrg(
     modifier: Modifier = Modifier,
     detailBrgUIState: DetailBrgUIState = DetailBrgUIState(),
     onDeleteClick: () -> Unit = { }
-){}
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailBrgUIState.isLoading->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
