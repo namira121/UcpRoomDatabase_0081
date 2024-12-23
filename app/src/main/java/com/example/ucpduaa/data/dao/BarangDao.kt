3728e230c4ce4d7b5ae877c58467ece03351ba27
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:37:14 +0700

    membuat fungsi barang untuk menambah data

--- app/src/main/java/com/example/ucpduaa/data/dao/BarangDao.kt
+++ app/src/main/java/com/example/ucpduaa/data/dao/BarangDao.kt
@@ -1,4 +1,13 @@
 package com.example.ucpduaa.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.example.ucpduaa.data.entity.Barang
+
+@Dao
 interface BarangDao {
+    @Insert
+    suspend fun insertbarang(
+        barang: Barang
+    )
 }
