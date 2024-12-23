57cd32f16aa6c4bc44ac5240bda63b06333242b6
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:15:29 +0700

    membuat suatu container untuk mengelola dependency dalam aplikasi

--- app/src/main/java/com/example/ucpduaa/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucpduaa/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,23 @@
 package com.example.ucpduaa.dependenciesinjection
 
+import android.content.Context
+import com.example.ucpduaa.data.database.TokoDatabase
+import com.example.ucpduaa.repository.LocalRepositoryBarang
+import com.example.ucpduaa.repository.LocalRepositorySuplier
+import com.example.ucpduaa.repository.RepositoryBarang
+import com.example.ucpduaa.repository.RepositorySuplier
+
+interface InterfaceContainerApp{
+    val repositoryBarang:RepositoryBarang
+    val repositorySuplier: RepositorySuplier
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp{
+    override val repositoryBarang: RepositoryBarang by lazy {
+        LocalRepositoryBarang(TokoDatabase.getDatabse(context).barangDao())
+    }
+    override val repositorySuplier: RepositorySuplier by lazy {
+        LocalRepositorySuplier(TokoDatabase.getDatabse(context).suplierDao())
+    }
+
+}
