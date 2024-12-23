aebd5c6e9e2375acbc689fbf000109a702416470
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:20:54 +0700

    membuat fungsi untuk customwidget TopAppBar beserta parameternya

--- app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,14 @@
 package com.example.ucpduaa.ui.customwidget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier
+){
+
+}
