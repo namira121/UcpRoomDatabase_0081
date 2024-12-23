a91f76553ab1dcc7bb469ef568d675c098fd9ead
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:02:03 +0700

    memanggil fungsi getallsuplier dari dao

--- app/src/main/java/com/example/ucpduaa/repository/RepositorySuplier.kt
+++ app/src/main/java/com/example/ucpduaa/repository/RepositorySuplier.kt
@@ -1,9 +1,10 @@
 package com.example.ucpduaa.repository
 
 import com.example.ucpduaa.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
 interface RepositorySuplier {
     suspend fun insertsuplier(suplier: Suplier)
 
-    fun getAllSuplier()
+    fun getAllSuplier(): Flow<List<Suplier>>
 }
