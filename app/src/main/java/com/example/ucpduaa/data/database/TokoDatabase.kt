e7fc5ea5f5acd2053e26ef918d0c97de78787b6c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:55:01 +0700

    membuat instance di roomdatabase

--- app/src/main/java/com/example/ucpduaa/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucpduaa/data/database/TokoDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucpduaa.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucpduaa.data.dao.BarangDao
 import com.example.ucpduaa.data.dao.SuplierDao
@@ -11,4 +13,20 @@ import com.example.ucpduaa.data.entity.Suplier
 abstract class TokoDatabase : RoomDatabase() {
     abstract fun barangDao(): BarangDao
     abstract fun suplierDao(): SuplierDao
+
+    companion object{
+        @Volatile
+        private var Instance: TokoDatabase? = null
+
+        fun getDatabse(context: Context): TokoDatabase{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    TokoDatabase::class.java,
+                    "TokoDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 }
