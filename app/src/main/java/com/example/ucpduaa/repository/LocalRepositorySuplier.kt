559ce5c08d04b688ac9b80ffd7cec21b50bf1d55
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:09:58 +0700

    mengimplementasikan fungsi getAllsuplier dari repositorysuplier dan dao

--- app/src/main/java/com/example/ucpduaa/repository/LocalRepositorySuplier.kt
+++ app/src/main/java/com/example/ucpduaa/repository/LocalRepositorySuplier.kt
@@ -2,11 +2,16 @@ package com.example.ucpduaa.repository
 
 import com.example.ucpduaa.data.dao.SuplierDao
 import com.example.ucpduaa.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositorySuplier(
     private val suplierDao: SuplierDao
 ): RepositorySuplier {
     override suspend fun insertsuplier(suplier: Suplier) {
         suplierDao.insertsuplier(suplier)
+    }
+
+    override fun getAllSuplier(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
     }
 }
