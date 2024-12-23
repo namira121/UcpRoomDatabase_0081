4eb5ab51f901a052a340e3191c1b9ffc2a06d9df
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:22:14 +0700

    mengatur layout box untuk TopAppBar

--- app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucpduaa/ui/customwidget/TopAppBar.kt
@@ -1,7 +1,12 @@
 package com.example.ucpduaa.ui.customwidget
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TopAppBar(
@@ -10,5 +15,12 @@ fun TopAppBar(
     judul: String,
     modifier: Modifier
 ){
+    Box(
+        modifier= Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ){
 
+    }
 }
