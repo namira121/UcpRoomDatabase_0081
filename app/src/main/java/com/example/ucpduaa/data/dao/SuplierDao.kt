c4a3dc03e02b4c4a52ce38a66ef8e6ab29a6a80a
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:45:16 +0700

    membuat fungsi getallsuplier  untuk menampilkan semua data suplier di database

--- app/src/main/java/com/example/ucpduaa/data/dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucpduaa/data/dao/SuplierDao.kt
@@ -2,7 +2,9 @@ package com.example.ucpduaa.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.ucpduaa.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SuplierDao {
@@ -10,4 +12,6 @@ interface SuplierDao {
     suspend fun insertsuplier(
         suplier: Suplier
     )
+    @Query("SELECT * FROM suplier ORDER BY namaspl ASC")
+    fun getAllSuplier(): Flow<List<Suplier>>
 }
