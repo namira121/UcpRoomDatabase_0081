40192933a6823519436fca28a24563487ef49f70
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 01:24:54 +0700

    membuat Floating Icon untuk edit dan memasukkan BodyDetail brf

--- app/src/main/java/com/example/ucpduaa/ui/view/DetailBrgView.kt
+++ app/src/main/java/com/example/ucpduaa/ui/view/DetailBrgView.kt
@@ -2,12 +2,17 @@ package com.example.ucpduaa.ui.view
 
 import android.app.AlertDialog
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -38,9 +43,24 @@ fun DetailBrgView(
                     onEditClick(viewModel.detailbrgUIState.value.detailUiEvent.id) },
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(16.dp)
-            ) { }
+            ) {
+                Icon(imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Barang")
+
+            }
         }
-    ) {  }
+    ) { innerPadding ->
+        val detailBrgUIState by viewModel.detailbrgUIState.collectAsState()
+
+        BodyDetailBrg(
+            modifier = Modifier.padding(innerPadding),
+            detailBrgUIState = detailBrgUIState,
+            onDeleteClick={
+                viewModel.deleteBrg()
+                onDeleteClick()
+            }
+        )
+    }
 }
 
 @Composable
