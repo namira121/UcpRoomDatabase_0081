de76d89ac17ad107f9edf3f7faa926c62b51e1c7
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 00:00:33 +0700

    membuat InsertBarangView dan menginisialisasikan viewmodelnya

--- app/src/main/java/com/example/ucpduaa/ui/view/InsertBarangView.kt
+++ app/src/main/java/com/example/ucpduaa/ui/view/InsertBarangView.kt
@@ -1,7 +1,22 @@
 package com.example.ucpduaa.ui.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucpduaa.ui.navigation.AlamatNavigasi
+import com.example.ucpduaa.ui.viewmodel.InsertBarangViewModel
+import com.example.ucpduaa.ui.viewmodel.PenyediaViewModel
 
 object DestinasiInsertBrg : AlamatNavigasi{
     override val route: String = "insertbarang"
+}
+
+@Composable
+fun InsertBarangView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertBarangViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
