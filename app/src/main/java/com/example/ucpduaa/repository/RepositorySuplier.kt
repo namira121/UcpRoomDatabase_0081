dc83077f225c2dd74a3fbe8bec0d275b514c627c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:01:31 +0700

    memanggil fungsi insertbarang dari dao

--- app/src/main/java/com/example/ucpduaa/repository/RepositorySuplier.kt
+++ app/src/main/java/com/example/ucpduaa/repository/RepositorySuplier.kt
@@ -1,4 +1,9 @@
 package com.example.ucpduaa.repository
 
+import com.example.ucpduaa.data.entity.Suplier
+
 interface RepositorySuplier {
+    suspend fun insertsuplier(suplier: Suplier)
+
+    fun getAllSuplier()
 }
