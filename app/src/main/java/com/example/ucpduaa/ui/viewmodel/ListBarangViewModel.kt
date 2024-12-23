4881f617bfb3b110a317a5edc43d5a1915f3a4c2
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 17:06:05 +0700

    membuat state in untuk mengubah flow menjadi stateflow

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/ListBarangViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/ListBarangViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.ucpduaa.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucpduaa.data.entity.Barang
 import com.example.ucpduaa.repository.RepositoryBarang
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class ListBrgUIState(
     val listBrg: List<Barang> = listOf(),
@@ -41,4 +44,11 @@ class ListBarangViewModel(
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ListBrgUIState(
+                isLoading = true
+            )
+        )
 }
