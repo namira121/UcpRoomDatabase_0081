e47898ad7a7168acdee653a2aada85fc429b231f
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:05:53 +0700

    mengimplementasikan fungsi insertbarang dari repositorybarang dan dao

--- app/src/main/java/com/example/ucpduaa/repository/LocalRepositoryBarang.kt
+++ app/src/main/java/com/example/ucpduaa/repository/LocalRepositoryBarang.kt
@@ -1,4 +1,12 @@
 package com.example.ucpduaa.repository
 
-class LocalRepositoryBarang {
+import com.example.ucpduaa.data.dao.BarangDao
+import com.example.ucpduaa.data.entity.Barang
+
+class LocalRepositoryBarang(
+    private val barangDao: BarangDao
+):RepositoryBarang {
+    override suspend fun insertbarang(barang: Barang) {
+        barangDao.insertbarang(barang)
+    }
 }
