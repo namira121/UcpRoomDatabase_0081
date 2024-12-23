ade3a33532edb4a7c4cf038022cc7358e987a501
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 17:29:58 +0700

    membuat fungsi creationextras

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.ucpduaa.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucpduaa.TokoApp
@@ -14,3 +16,6 @@ object PenyediaViewModel{
         }
     }
 }
+
+fun CreationExtras.TokoApp(): TokoApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
