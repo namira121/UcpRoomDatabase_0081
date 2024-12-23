ac24d37703421fa5e863a8beb646c1c3cd9bb584
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 13:49:23 +0700

    membuat database TokoDatabase dengan entitas dan dao yang sudah dibuat

--- app/src/main/java/com/example/ucpduaa/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucpduaa/data/database/TokoDatabase.kt
@@ -1,4 +1,14 @@
 package com.example.ucpduaa.data.database
 
-class TokoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ucpduaa.data.dao.BarangDao
+import com.example.ucpduaa.data.dao.SuplierDao
+import com.example.ucpduaa.data.entity.Barang
+import com.example.ucpduaa.data.entity.Suplier
+
+@Database(entities = [Barang::class], [Suplier::class], version = 1, exportSchema = false)
+abstract class TokoDatabase : RoomDatabase() {
+    abstract fun barangDao(): BarangDao
+    abstract fun suplierDao(): SuplierDao
 }
