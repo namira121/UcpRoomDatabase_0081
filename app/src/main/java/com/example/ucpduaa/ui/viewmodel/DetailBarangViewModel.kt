2a991445fd44b58958249dc3f9edc3b51b1afe94
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 21:05:11 +0700

    membuat state untuk state error dan stateIn untuk mengubah flow menjadi stateflow

--- app/src/main/java/com/example/ucpduaa/ui/viewmodel/DetailBarangViewModel.kt
+++ app/src/main/java/com/example/ucpduaa/ui/viewmodel/DetailBarangViewModel.kt
@@ -2,12 +2,16 @@ package com.example.ucpduaa.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucpduaa.data.entity.Barang
 import com.example.ucpduaa.repository.RepositoryBarang
 import com.example.ucpduaa.ui.navigation.DestinasiDetailBarang
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 fun Barang.toDetailEvent(): BarangEvent{
     return BarangEvent(
@@ -47,4 +51,20 @@ class DetailBarangViewModel (
                 isLoading = false,
             )
         }
+        .catch {
+            emit(
+                DetailBrgUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?:"Tejadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailBrgUIState(
+                isLoading = true,
+            )
+        )
 }
