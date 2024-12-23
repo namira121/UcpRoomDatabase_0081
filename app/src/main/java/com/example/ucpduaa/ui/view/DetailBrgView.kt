3f309d7ef610e7d9aecbe7bcce66c217fca7d04c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 01:08:21 +0700

    membuat dialog untuk konfirmasi delete

---
+++ app/src/main/java/com/example/ucpduaa/ui/view/DetailBrgView.kt
@@ -0,0 +1,30 @@
+package com.example.ucpduaa.ui.view
+
+import android.app.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text={ Text("Apaka anda ingin manghapus data ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+        )
+}
