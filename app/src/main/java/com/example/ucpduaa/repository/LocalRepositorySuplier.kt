d9ed5eab0db0f6c48564c248f0d5ca1bc3ccb3c8
Namira Pramadhani <namnam6617@gmail.com>
2024-12-23 14:09:29 +0700

    mengimplementasikan fungsi insertsuplier dari repositorysuplier dan dao

--- app/src/main/java/com/example/ucpduaa/repository/LocalRepositorySuplier.kt
+++ app/src/main/java/com/example/ucpduaa/repository/LocalRepositorySuplier.kt
@@ -1,4 +1,12 @@
 package com.example.ucpduaa.repository
 
-class LocalRepositorySuplier {
+import com.example.ucpduaa.data.dao.SuplierDao
+import com.example.ucpduaa.data.entity.Suplier
+
+class LocalRepositorySuplier(
+    private val suplierDao: SuplierDao
+): RepositorySuplier {
+    override suspend fun insertsuplier(suplier: Suplier) {
+        suplierDao.insertsuplier(suplier)
+    }
 }
