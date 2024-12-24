fefe0d63f03a94d366f0bc1c915047c9c9521ebd
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 08:29:32 +0700

    memasukkan insertbodybrg dan membuatnya untuk menyimpan data yang sudah diupdate

--- app/src/main/java/com/example/ucpduaa/ui/view/UpdateBarangView.kt
+++ app/src/main/java/com/example/ucpduaa/ui/view/UpdateBarangView.kt
@@ -1,16 +1,26 @@
 package com.example.ucpduaa.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import com.example.ucpduaa.ui.customwidget.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucpduaa.ui.viewmodel.PenyediaViewModel
 import com.example.ucpduaa.ui.viewmodel.UpdateBarangViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateBarangView(
@@ -35,6 +45,44 @@ fun UpdateBarangView(
                 )
                 viewModel.resetSnackBarMessage()
             }
+        }
+    }
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+
+        topBar = {
+            TopAppBar(
+                judul = "Edit Barang",
+                showBackButton = true,
+                modifier = modifier,
+                onBack = onBack
+            )
+        }
+    ){padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyBrg(
+                uiState = uiState,
+                onValueChange = {updatedEvent ->
+                    viewModel.UpdateState(updatedEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.ValidateFields()){
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main){
+                                onNavigate()
+                            }
+                        }
+                    }
+                }
+            )
         }
     }
 }
