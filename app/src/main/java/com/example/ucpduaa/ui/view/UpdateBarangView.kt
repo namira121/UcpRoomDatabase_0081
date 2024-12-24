26a5bfc0023014bee42dfffb230a1851eae563d3
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 08:19:52 +0700

    membuat launchedeffect di UpdateBarangView untuk snackbar

---
+++ app/src/main/java/com/example/ucpduaa/ui/view/UpdateBarangView.kt
@@ -0,0 +1,40 @@
+package com.example.ucpduaa.ui.view
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucpduaa.ui.viewmodel.PenyediaViewModel
+import com.example.ucpduaa.ui.viewmodel.UpdateBarangViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun UpdateBarangView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateBarangViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.updateUIState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+}
