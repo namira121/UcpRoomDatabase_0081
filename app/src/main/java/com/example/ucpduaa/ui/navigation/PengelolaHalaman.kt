d6b9089c2baf9bdd811d9c77b2c260d09a5e66cf
Namira Pramadhani <namnam6617@gmail.com>
2024-12-24 07:47:56 +0700

    membuat route untuk fungsi click yang ada di button

--- app/src/main/java/com/example/ucpduaa/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ucpduaa/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,40 @@
 package com.example.ucpduaa.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ucpduaa.ui.view.DestinasiInsertBrg
+import com.example.ucpduaa.ui.view.DestinasiInsertSpl
+import com.example.ucpduaa.ui.view.HomeView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController= rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route){
+        composable(
+            route = DestinasiHome.route
+        ){
+            HomeView(
+                onAddBarang = {
+                    navController.navigate(DestinasiInsertBrg.route)
+                },
+                onAddSuplier = {
+                    navController.navigate(DestinasiInsertSpl.route)
+                },
+                onListBarang = {
+                    navController.navigate(DestinasiBarang.route)
+                },
+                onListSuplier = {
+                    navController.navigate(DestinasiSuplier.route)
+                }
+            )
+        }
+    }
+}
